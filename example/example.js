0377b7df1d41cde27f746e2e856073ccac80064b
HamzaMohammed89 <mdhamzanaveed@gmail.com>
2025-08-04 00:34:40 -0400

    Add example

---
+++ example/example.js
@@ -0,0 +1,16 @@
+const { processdataset } = require('./index');
+
+let dataset = 'dataset file path';
+
+const result = processdataset({
+        dataset, 
+        numeric_threshold :0.7, 
+        remove_constant_columns: true, 
+        remove_duplicate_columns: true, 
+        remove_correlated_columns: true, 
+        correlation_coefficient_magnitude: 0.8
+});
+
+console.log("✅ Cleaned Dataset:", result.data);
+console.log("📊 Processing Results:", result.processing_results);
+console.log("⚙️ Processing Selection:", result.processing_selection);
